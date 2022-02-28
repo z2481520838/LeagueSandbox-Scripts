771ad3ccfb03c5d96e682513a2a3026cddbac1aa
Cosmin <gusto_cs@yahoo.com>
2022-02-28 23:49:00 +0200

    Katarina's buffs
    Complete Katarina Buffs

---
+++ Buffs/Katarina/WHaste.cs
@@ -0,0 +1,38 @@
+using GameServerCore.Enums;
+using GameServerCore.Domain.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+
+namespace Buffs
+{
+    internal class KatarinaWHaste : IBuffGameScript
+    {
+        public BuffType BuffType => BuffType.HASTE;
+        public BuffAddType BuffAddType => BuffAddType.REPLACE_EXISTING;
+        public int MaxStacks => 1;
+        public bool IsHidden => false;
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        IParticle p;
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            StatsModifier.MoveSpeed.PercentBonus = StatsModifier.MoveSpeed.PercentBonus + (0.10f + 0.05f * ownerSpell.CastInfo.SpellLevel);
+            unit.AddStatModifier(StatsModifier);
+            //p = AddParticleTarget(ownerSpell.CastInfo.Owner, unit, "Overdrive_buf.troy", unit, buff.Duration);
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            RemoveParticle(p);
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
+
